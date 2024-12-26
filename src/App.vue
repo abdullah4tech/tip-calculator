3fb3e457840c84ffb85f48e0d94088ea71eded67
abdullah4tech <a41622100@gmail.com>
2024-12-26 02:19:06 +0000

    feat: finished project

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
 import iconLogo from './Icons/icon-logo.vue'
 import IconDollar from './Icons/icon-dollar.vue'
 import iconPerson from './Icons/icon-person.vue'
-import { computed, ref } from 'vue';
-
+import { computed, ref } from 'vue'
 
 const selectedTip = ref(null)
 const number_people = ref(null)
@@ -11,14 +10,13 @@ const custom_input = ref(null)
 const billAmount = ref(null)
 
 const tipAmountPerPerson = computed(() => {
-  const tipPercentage = custom_input.value || selectedTip.value || 0;
-  if (!billAmount.value || !number_people.value || number_people.value <= 0) return 0;
-  return (billAmount.value * tipPercentage) / (100 * number_people.value);
-});
-
+  const tipPercentage = custom_input.value || selectedTip.value || 0
+  if (!billAmount.value || !number_people.value || number_people.value <= 0) return 0
+  return (billAmount.value * tipPercentage) / (100 * number_people.value)
+})
 
 const TotalPerPerson = computed(() => {
-  return (billAmount.value / number_people.value) + tipAmountPerPerson.value
+  return billAmount.value / number_people.value + tipAmountPerPerson.value
 })
 
 const ResetInput = () => {
@@ -32,13 +30,14 @@ const deb = (i) => {
   selectedTip.value = i
   console.log(selectedTip.value)
 }
-
 </script>
 
 <template>
   <div class="flex md:flex-col flex-col gap-16 mt-64 md:mt-0 md:gap-16 items-center">
     <iconLogo />
-    <div class="flex flex-col md:flex-row md:gap-14 md:w-[500px] gap-14 p-7 md:p-9 rounded-3xl bg-white">
+    <div
+      class="flex flex-col md:flex-row md:gap-14 md:w-[500px] gap-14 p-7 md:p-9 rounded-3xl bg-white"
+    >
       <!-- Content here -->
 
       <!-- Start Input -->
@@ -47,7 +46,12 @@ const deb = (i) => {
           <label for="bills">Bill</label>
           <div class="flex items-center p-2 rounded space-x-[40px] px-4 gap-3 input-feild">
             <IconDollar />
-            <input id="bills" v-model="billAmount" class="outline-none bg-transparent text-right" type="number" />
+            <input
+              id="bills"
+              v-model="billAmount"
+              class="outline-none bg-transparent text-right"
+              type="number"
+            />
           </div>
         </div>
         <div class="flex flex-col gap-3">
@@ -56,7 +60,7 @@ const deb = (i) => {
             <h2
               :class="[
                 'md:p-[5px] p-3 rounded cursor-pointer text-white font-bold',
-                selectedTip === i ? 'bg-ver-dark-cyan' : 'bg-cyan-900'
+                selectedTip === i ? 'bg-ver-dark-cyan' : 'bg-cyan-900',
               ]"
               v-for="(i, index) in [5, 10, 15, 25, 50]"
               @click="deb(i)"
@@ -77,11 +81,25 @@ const deb = (i) => {
         <div class="flex flex-col gap-2">
           <div class="flex items-center justify-between">
             <label for="num_people">Number of People</label>
-            <span v-show="number_people === 0" :class="'text-xs text-red-600' + (number_people === 0 ? '' : '')">Can't be zero</span>
+            <span
+              v-show="number_people === 0"
+              :class="'text-xs text-red-600' + (number_people === 0 ? '' : '')"
+              >Can't be zero</span
+            >
           </div>
-          <div :class="'flex items-center rounded space-x-[40px] p-2 px-4 gap-3 input-feild' + (number_people === 0 ? ' border-red-600 border-[0.5px]' : '')">
+          <div
+            :class="
+              'flex items-center rounded space-x-[40px] p-2 px-4 gap-3 input-feild' +
+              (number_people === 0 ? ' border-red-600 border-[0.5px]' : '')
+            "
+          >
             <iconPerson />
-            <input id="num_people" class="outline-none bg-transparent text-right" v-model="number_people" type="number" />
+            <input
+              id="num_people"
+              class="outline-none bg-transparent text-right"
+              v-model="number_people"
+              type="number"
+            />
           </div>
         </div>
       </div>
@@ -94,14 +112,18 @@ const deb = (i) => {
               <p>Tip Amount</p>
               <span>/ person</span>
             </div>
-            <h1 class="md:text-4xl text-3xl">${{ tipAmountPerPerson ? tipAmountPerPerson.toFixed(2) : '0.00' }}</h1>
+            <h1 class="md:text-4xl text-3xl">
+              ${{ tipAmountPerPerson ? tipAmountPerPerson.toFixed(2) : '0.00' }}
+            </h1>
           </div>
           <div class="flex items-center gap-28">
             <div class="text-xs text-new">
               <p>Total</p>
               <span>/ person</span>
             </div>
-            <h1 class="md:text-4xl text-3xl">${{ TotalPerPerson ? TotalPerPerson.toFixed(2) : '0.00' }}</h1>
+            <h1 class="md:text-4xl text-3xl">
+              ${{ TotalPerPerson ? TotalPerPerson.toFixed(2) : '0.00' }}
+            </h1>
           </div>
         </div>
         <button class="ll p-2 text-sm outline-none" @click="ResetInput" type="reset">RESET</button>
@@ -145,8 +167,6 @@ button {
 button:hover {
   background-color: hsl(186, 19%, 89%); /* Very dark cyan */
 }
-
-
 
 /* Tip Amount Display */
 .text-new {
